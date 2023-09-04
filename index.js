bf621435d7a09e9f0fb4a6e8079fc89646e30b34
Dadams11 <adams.1743@osu.edu>
2023-09-03 23:16:32 -0400

    updated index.js file added middleware for JSOB parsing and error handling

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const apiRoutes = require('./routes/api');
 
 const app = express();
+
+// Middleware for JSON parsing
 app.use(express.json());
 
+// Debugging middleware
+app.use((req, res, next) => {
+  console.log('Incoming Request Data:', req.body);
+  next();
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error(err);
+    res.status(400).send({ error: 'Invalid JSON format' });
+  } else {
+    next();
+  }
+});
+
+// Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/socialNetwork', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,6 +34,10 @@ db.once('open', () => {
   console.log('MongoDB connected');
 });
 
+// Import and use API routes
+app.use('/api', apiRoutes);
+
+// Start the server
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
